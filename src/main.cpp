fd3a92d569bc4b3e6e0a5981edbeab88b59aa6ef
DimKush <demonmadman228@gmail.com>
2020-01-18 02:15:12 +0300

    Create new branch

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 int main()
 {
     std::cout << "build = " << PROJECT_VERSION_PATCH << std::endl;
-    std::cout << "Hello world and travis!";
+    std::cout << "Hello world and travis!" << std::endl;
+    std::cout << "Branch is fixOne" << std::endl;
     return 0;
 }
